c2d4ae2fcd7bf1075504f40a8651ddfb7620a609
Jeff Lowy <jeffreylowy@gmail.com>
2017-08-22 07:31:16 -0700

    CSS: added .hint .word. | JS: Var for #game. Added words & hints to dictionary. Added listen method to startGame. newGame method.

--- assets/js/got-hangman.js
+++ assets/js/got-hangman.js
@@ -7,8 +7,26 @@ var hangman = {};
 
 hangman.playing = false;
 
+hangman.gameElement = "#game";
+
 // Create the array of items the user will guess.
-hangman.dictionary = ["Ned", "Catelyn", "Jon Snow"];
+hangman.dictionary = [{
+        word: "Valyrian",
+        hint: "This steel is recognizable by its strength and light weight in comparison to ordinary steel, as well as by a distinctive rippled pattern visible in blades made from it."
+    },
+    {
+        word: "Blindness",
+        hint: "Arya's punishment for stealing from the Many-Face God."
+    },
+    {
+        word: "Daenerys",
+        hint: "First of Her Name, the Unburnt, Queen of the Andals and the First Men, Khaleesi of the Great Grass Sea, Breaker of Chains, and Mother of Dragons."
+    },
+    {
+        word: "Arya",
+        hint: "trained as a Faceless Man at the House of Black and White in Braavos."
+    }
+];
 
 // Array of background image numbers
 hangman.backgrounds = ["01", "02", "03", "04", "05", "06"];
@@ -44,22 +62,54 @@ hangman.gifs = {
 
 /* ------ HANGMAN METHODS ------ */
 
-hangman.startGame = function(selector) {
-    var isPlaying = this.playing;
+// Takes the div#game element as a parameter 
+hangman.startGame = function (selector) {
+
+    //Store reference to the hangman object and div#game in a variable. 
+    var self = this;
     var element = document.querySelector(selector);
-    document.addEventListener("keyup", function(event) {
-        if (event.key === 'Enter' && !isPlaying) {
-            element.className += "fadeInStart";
-            isPlaying = true;
-        }
-        else if (event.key === 'Enter' && isPlaying) {
+
+    // Add a listener for the ENTER key. Add fadeInUp class to trigger the animation. Set hangman.playing to true.
+    document.addEventListener("keyup", function (event) {
+        if (event.key === 'Enter' && !self.playing) {
+            element.className += "fadeInUp";
+            self.playing = true;
+
+            // Listen for the animations on div#game to complete. Then run hangman.newGame.
+            self.listen(hangman.gameElement, "animationend", function (param) {
+                self.newGame();
+                console.log("Animation complete:", param.animationName);
+            });
+        } else if (event.key === 'Enter' && isPlaying) {
             console.log("You are already playing a game.");
         }
     });
 }
 
+hangman.newGame = function () {
+    var item = hangman.randomize(hangman.dictionary);
+    var word = item.word;
+    var hint = item.hint;
+
+    var gameBoard = document.querySelector(hangman.gameElement);
+    var hintElement = document.createElement("h1");
+    hintElement.className += 'hint';
+    hintElement.innerHTML = hint;
+
+    var wordElement = document.createElement("h1");
+    wordElement.className += 'word';
+    wordElement.innerHTML = word;
+
+    gameBoard.appendChild(hintElement);
+    gameBoard.appendChild(wordElement);
+
+    // Log the word and hint to the console for testing (aka cheating).
+    console.log("WORD:", word);
+    console.log("HINT:", hint);
+}
+
 // A function used to ranomize the dictionary array, the background images, and the win/loss gifs.
-hangman.randomize = function(arr) {
+hangman.randomize = function (arr) {
     var numberOfItems = arr.length - 1;
     var min = Math.ceil(0);
     var max = Math.floor(numberOfItems);
@@ -67,13 +117,13 @@ hangman.randomize = function(arr) {
 }
 
 // Sets a random background image from the hangman.backgrounds array.
-hangman.setBackground = function(selector) {
+hangman.setBackground = function (selector) {
     var element = document.querySelector(selector);
     var bg = hangman.randomize(hangman.backgrounds);
     element.style.backgroundImage = 'url("assets/images/got-bg-' + bg + '.jpg")';
 }
 
-hangman.statusImage = function(selector, status) {
+hangman.statusImage = function (selector, status) {
     var element = document.querySelector(selector);
     var img = document.createElement("img");
     var winOrLose = status === 'win' ? hangman.randomize(hangman.gifs.win) : hangman.randomize(hangman.gifs.lose);
@@ -83,16 +133,16 @@ hangman.statusImage = function(selector, status) {
 }
 
 // Custom listen function tht takes an element, an event and a callback function.
-hangman.listen = function(selector, event, callback) {
+hangman.listen = function (selector, event, callback) {
     var element = document.querySelector(selector);
     element.addEventListener(event, callback, false);
 }
 
 /* ------ INITIALIZE GAME ------ */
 
-hangman.startGame("#game");
+hangman.startGame(hangman.gameElement);
 
-hangman.listen("h1.title", "animationend", function(param) {
+hangman.listen("h1.title", "animationend", function (param) {
     console.log("Animation complete:", param.animationName);
 });
 

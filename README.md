850e631078a2c037995ec95c48aa3f408f23d258
Jeff Lowy <jeffreylowy@gmail.com>
2017-08-26 00:32:39 -0700

    Ready for submission

--- README.md
+++ README.md
@@ -1,14 +1,62 @@
 # Hangman: Game of Thrones Edition
 Hangman game homework for UCB bootcamp
 
+### The Game
+- Press enter to begin
+- In case you're unfamiliar with Game of Thrones, the word is logged to the console using console.warn
+- You only need to press a letter once should it appear multiple times in the word.
+- During each game session, keys are stored in an array when pressed. The array is evaluated on each press. Repeated keys are ignored. A correct guess will reveal a letter(s), and incorrect guesses remove a heart.
+- The score is tracked throughout each game session.
+- The win/loss count is stored in the hangman object and displayed at the end of each game.
+- A .gif from Giphy loads based on the outcome of the game.
+- A new game begins 10 seconds after the previous game finishes. 
+
+## Recap
+#### CSS/Layout
+I knew from the start I wanted to make it a Game of Thrones (GoT) theme. I started out with the design. I wanted to make it responsive, or at least make everything fluid based on the browsers width and height. I accomplished this mostly using vh/vw units for width and height on everything. I used the [Flexible Math](http://responsv.com/flexible-math/) site to help me create the ratios for each element. The only piece that isn't responsive/fluid is the title, though I did read up a bit on [responsive typography](https://zellwk.com/blog/responsive-typography).
+
+I downloaded TTF fonts for the generic GoT font and used [WOFFer – WOFF font converter](https://andrewsun.com/tools/woffer-woff-font-converter/) to convert the fonts for use on the page. The other fonts rely on Google Fonts and use Times as a fallback.
+
+Finally, I like the animations provided by the animation.css library, but I did not want to include the library in the page for just the two animations I planned to use. So I dug into their source code and pulled out the animations I needed. [1]() [2]() 
+
+#### JS & Chrome Dev Tools
+
+I went in from the start knowing I wanted to accomplish all of the bonus tasks. Things started out easier enough. As the size of the code grew, and crazier the logic became, I started running into issues with `keyup` events conflicting, variables being undefined when they should have values and variables not updating when they should. Also, if you go through the js code, you'll see that the hangman object is the only global object. There are few places where I store `this` (the hangman object) in a variable named self so I can access the methods of the hangman object in nested functions.
+
+I learned a few cool tricks with Dev Tools:
+
+1. [Adding CSS to console.log statments](https://coderwall.com/p/fskzdw/colorful-console-log) - You can see this [here]() or in the console on keyup events and when animations complete.
+2. [Setting breakpoints in the Sources panel](https://developers.google.com/web/tools/chrome-devtools/javascript/breakpoints)
+
+## Challenges I set for myself
+1. Make page responsive/fluid - at the very least I wanted a background that adapted to the window size.
+2. Trigger events when animations complete (title, press start, console.logs)
+3. Use ESLint with no errors
+4. Complete the bonus items in the homework instructions
+5. Get a better understanding of the Dev Tools outside of the console and elements tabs.
+6. Keep practicing CSS positioning (the dark red sigils in the four corners of the game board)
+7. Choose a random background on page load.
+
 ## Resources 
+[Flexible Math](http://responsv.com/flexible-math/)
+
+[WOFFer – WOFF font converter](https://andrewsun.com/tools/woffer-woff-font-converter/)
+
+[Google Developers - Pause Your Code With Breakpoints](https://developers.google.com/web/tools/chrome-devtools/javascript/breakpoints)
+
 [MDN - Using CSS animations: Adding the animation event listeners](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Using_CSS_animations#Adding_the_animation_event_listeners)
 
 [MDN - Document.querySelector()](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector)
 
 [MDN - Conditional (ternary) Operator](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator)
 
+[MDN - Audio](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio)
+
+[Colorful console.log](https://coderwall.com/p/fskzdw/colorful-console-log)
+
 [Stack Overflow - How to find index of all occurrences of element in array?](https://stackoverflow.com/questions/20798477/how-to-find-index-of-all-occurrences-of-element-in-array)
+
+[Stack Overflow - How to remove an HTML element using Javascript?](https://stackoverflow.com/questions/5933157/how-to-remove-an-html-element-using-javascript)
 
 [A List Apart: More Meaningful Typography](https://alistapart.com/article/more-meaningful-typography)
 
--- assets/js/got-hangman.js
+++ assets/js/got-hangman.js
@@ -9,11 +9,21 @@ hangman.gameElement = "#game";
 
 hangman.playing = false;
 
+hangman.state = {
+    playing: false,
+    score: 0,
+    wins: 0,
+    losses: 0,
+    word: "",
+    statusElement: ".status"
+}
+
 hangman.hearts = {
     numHearts: 0,
     heartsElement: "#hearts",
-    generateHearts: function (word, element) {
-        for (var l = 0; l < (word.length * 2); l++) {
+    generateHearts: function (lives, element) {
+        for (var l = 0; l < lives; l++) {
+            // Wrapping each image created in the loop an IFFE, creating a closure around each pixelHeart variable.
             (function () {
                 var pixelHeart = document.createElement("img");
                 pixelHeart.src = "assets/images/pixel-heart.png";
@@ -21,16 +31,29 @@ hangman.hearts = {
                 element.appendChild(pixelHeart);
             }());
         }
+        element.style.display = "block";
+    },
+    removeHeart: function (className) {
+        var hearts = document.querySelectorAll(className);
+        var heartContainer = hearts[hearts.length - 1].parentNode;
+
+        // console.log("HEART CONTAINER", heartContainer);
+
+        heartContainer.removeChild(hearts[hearts.length - 1]);
     }
 };
 
 hangman.word = {
+    numGuesses: 0,
+    currentWord: "",
+    guessArray: [],
     maskElement: "#mask",
     hintElement: "#hint",
     // Takes the word and html element as parameters
+    // Like the pixelHeart mehtod above, maskWord creates a closure around each letterMask var.
     maskWord: function (word, element) {
+
         for (var l = 0; l < word.length; l++) {
-            console.log("l is:", l, word[l]);
 
             (function () {
                 var letterMask = document.createElement("span");
@@ -39,11 +62,7 @@ hangman.word = {
                 element.appendChild(letterMask);
             }());
         }
-        console.log(mask);
-    },
-    getLetters: function (className) {
-        var letters = document.querySelectorAll(className);
-        console.log("hagman.word.getLetters:", letters);
+        element.style.display = "block";
     },
     getIndexes: function (arr, val) {
         var indexes = [];
@@ -68,7 +87,43 @@ hangman.word = {
         },
         {
             word: "Arya",
-            hint: "trained as a Faceless Man at the House of Black and White in Braavos."
+            hint: "Trained as a Faceless Man at the House of Black and White in Braavos."
+        },
+        {
+            word: "Joffrey",
+            hint: "Ned Stark was beheaded by which notorious character"
+        },
+        {
+            word: "Lannister",
+            hint: "A ____________ always pays his debts."
+        },
+        {
+            word: "Dragons",
+            hint: "Daenerys Stormborn of the House Targaryen is also known as The Mother of..."
+        },
+        {
+            word: "Seven",
+            hint: "How many kingdoms are in Westeros?"
+        },
+        {
+            word: "Ned",
+            hint: "Which character originally says, 'The man who passes the sentence should swing the word'?"
+        },
+        {
+            word: "Bronn",
+            hint: "Jaime Lannister's (previously Tyrion Lannister's) right-hand man is..."
+        },
+        {
+            word: "Ladder",
+            hint: "Chaos isn't a pit, it's..."
+        },
+        {
+            word: "Wights",
+            hint: "What is the name given to those who are resurrected by the White Walkers?"
+        },
+        {
+            word: "Cersei",
+            hint: "Which character says, 'When you play the game of thrones, you win or you die. There is no middle ground.'?"
         }
     ]
 }
@@ -114,24 +169,76 @@ hangman.startGame = function (selector) {
     var self = this;
     var element = document.querySelector(selector);
 
+    // Listen fot title animation to complete.
+    this.animationListener("h1.title", "animationend", function (paramFromEventListener) {
+        
+        // Display 'Press Enter'
+        document.querySelector(".press-start").className += " flash";
+
+        // Play Game of Thrones theme music.
+        self.audioElement();
+        console.log("%cAnimation complete: " + paramFromEventListener.animationName, "background-color: green; color:white; font-weight:bold; display:block;");
+    });
+
+    // Create a reference to the div#hearts, div#mask and div#hint
+    var heartsElement = document.querySelector(this.hearts.heartsElement);
+    var maskElement = document.querySelector(this.word.maskElement);
+    var hintElement = document.querySelector(this.word.hintElement);
+    var pressStart = document.querySelector(".press-start");
+
+
     // Add a listener for the ENTER key. Add fadeInUp class to trigger the animation. Set hangman.playing to true.
     document.addEventListener("keyup", function (event) {
+
+        pressStart.style.display = "none";
 
         var key = event.key;
 
         if (key === 'Enter' && !self.playing) {
-            element.className += "fadeInUp";
+            element.className += "fadeInUpBig";
             self.playing = true;
 
             // Listen for the animations on div#game to complete. Then run hangman.newGame.
-            self.listen(self.gameElement, "animationend", function (param) {
+            self.animationListener(self.gameElement, "animationend", function (paramFromEventListener) {
                 self.newGame();
-                console.log("Animation complete:", param.animationName);
+                console.log("%cAnimation complete: " + paramFromEventListener.animationName, "background-color: green; color:white; font-weight:bold; display:block;");
             });
         } else if (key === 'Enter' && self.playing) {
             // If the player is already in game and hits enter, console.log the message below.
-            console.log("Stop pressing Enter. You are already playing a game.");
+            console.warn("You are already playing a game.");
+        } else {
+            var key = event.key.toLowerCase();
+            var hasKey = self.state.word.indexOf(key) !== -1 ? true : false;
+            console.log("VALID KEY:", hasKey);
+
+            // Use the console.count method to display the number of times a key has been pressed.
+            console.count(key + " keyup count");
+
+            if (self.word.guessArray.indexOf(key) === -1) {
+                // Push the key pressed into the guessArray
+                self.word.guessArray.push(key);
+
+                if (key !== 'Enter' && self.playing && hasKey) {
+                    self.state.score = self.correctKey(key, self.state.word, self.state.score);
+                    if (self.state.score === self.state.word.length) {
+                        console.warn("You win!");
+                        self.state.wins++
+                            self.gameComplete(heartsElement, hintElement, maskElement, "win");
+                        self.word.guessArray = [];
+                    }
+                } else {
+                    self.hearts.removeHeart(".pixelheart");
+                    self.hearts.numHearts--;
+                    if (self.hearts.numHearts === 0) {
+                        console.warn("You lose!");
+                        self.state.losses++
+                            self.gameComplete(heartsElement, hintElement, maskElement, 'lose');
+                    }
+                    console.log("%cNumber of lives " + self.hearts.numHearts, "background-color: black; color:white; font-weight:bold; display:block;");
+                }
+            }
         }
+
     });
 
 }
@@ -139,50 +246,55 @@ hangman.startGame = function (selector) {
 // Setup a new game
 hangman.newGame = function () {
 
-    // Creating a reference to this (hangman) so I can access the hangman.playing status in the event listener at the bottom of this method.
-    var self = this;
-
     // Uses my randomize function to grab a random item from hangman.word.dictionary 
     var item = this.randomize(this.word.dictionary);
-    var word = item.word.toLowerCase();
+    this.state.word = item.word.toLowerCase();
     var hint = item.hint.toLowerCase();
-
-    // Create a reference to the div#game. 
-    var gameElement = document.querySelector(this.gameElement);
+    this.hearts.numHearts = parseInt(this.state.word.length);
+    this.state.score = 0;
+    this.word.guessArray = [];
 
     // Create a reference to the div#hearts, div#mask and div#hint
     var heartsElement = document.querySelector(this.hearts.heartsElement);
     var maskElement = document.querySelector(this.word.maskElement);
     var hintElement = document.querySelector(this.word.hintElement);
+    var statusElement = document.querySelector(".status");
+    statusElement.style.display = "none";
+    heartsElement.innerHTML = "";
+    maskElement.innerHTML = "";
+    statusElement = "";
 
-    // Call hangman.hearts generateHearts method. Pass in the random word and the div#hearts.
-    this.hearts.generateHearts(word, heartsElement);
-    this.word.maskWord(word, maskElement);
+    // Call hangman.hearts generateHearts method. Pass in the number of lives (guesses) and div#hearts.
+    this.hearts.generateHearts(this.hearts.numHearts, heartsElement);
+
+    // Call hangman.maskWord method. Pass in the random word and div#mask.
+    this.word.maskWord(this.state.word, maskElement);
     hintElement.textContent = hint;
+    hintElement.style.display = "block"
 
-    // Testing hangman.word.getLetters
-    hangman.word.getLetters(".lettermask");
     // Log the word and hint to the console for testing (aka cheating).
-    console.log("**** WORD ****", word);
-    console.log("**** HINT ****", hint);
+    console.warn("**** WORD ****", this.state.word);
+    console.warn("**** HINT ****", hint);
 
-    document.addEventListener("keyup", function (event) {
-        var key = event.key.toLocaleLowerCase();
-        var indexes = [];
-        var hasKey = word.indexOf(key) !== -1 ? true : false;
-        console.log(hasKey);
-        if (key !== 'Enter' && self.playing && hasKey) {
-            key = key.toLowerCase();
-            console.log("Key Pressed:",key);
-            indexes = self.word.getIndexes(word, key);
-            indexes.forEach(function(item){
-                console.log("index item:",item);
-                document.querySelectorAll(".lettermask")[item].textContent = key;
-            });
-        } else {
-            console.log("NOT VALID");
-        }
+
+}
+
+hangman.correctKey = function (key, word, currentScore) {
+
+    console.log("Key Pressed:", key);
+    var indexes = this.word.getIndexes(word, key);
+    var maskedLetters = document.querySelectorAll(".lettermask");
+    indexes.forEach(function (item) {
+        console.log("index item:", item);
+        maskedLetters[item].textContent = key;
     });
+    console.log("Current Score:", currentScore);
+    var newScore = parseInt(currentScore + indexes.length);
+    console.log("points added:", indexes.length);
+    console.log("%cScore  " + newScore, "background-color: blue; color:white; font-weight:bold; display:block;");
+
+    return newScore;
+
 }
 
 // A function used to ranomize the dictionary array, the background images, and the win/loss gifs.
@@ -191,6 +303,73 @@ hangman.randomize = function (arr) {
     var min = Math.ceil(0);
     var max = Math.floor(numberOfItems);
     return arr[Math.floor(Math.random() * (max - min + 1)) + min];
+}
+
+hangman.gameComplete = function (heartsElement, hintElement, maskElement, result) {
+
+    // Make a reference to the hangman object so newGame() can be called inside setTimeout.
+    var self = this;
+
+    // store the HTML game elements in an array for the forEach method below.
+    var elements = [heartsElement, hintElement, maskElement];
+    var status = document.querySelector(this.state.statusElement);
+
+    // Create win/loss/counter elements to be appended in the status element.
+    // WINS
+    var winsElement = document.createElement("h3");
+    var winsSpan = document.createElement("span");
+    winsElement.textContent = "Wins: ";
+    winsSpan.textContent = this.state.wins;
+    winsElement.appendChild(winsSpan);
+    // LOSSES
+    var losesElement = document.createElement("h3");
+    var losesSpan = document.createElement("span");
+    losesElement.textContent = "Losses: ";
+    losesSpan.textContent = this.state.losses;
+    losesElement.appendChild(losesSpan);
+    // COUNTER
+    var counterElement = document.createElement("h5");
+    var counterSpan = document.createElement("span");
+    counterElement.textContent = "Next game: ";
+    counterSpan.textContent = 10;
+    counterElement.appendChild(counterSpan);
+
+    // Clear the html and set to display:none for each of the items in the elements array.
+    elements.forEach(function (element) {
+        element.innerHTML = "";
+        element.style.display = "none";
+    });
+
+    // Display a win/loss gif to the status element.
+    this.statusImage(this.state.statusElement, result);
+
+    // Reset the score and guessArray, which are used for each game session.
+    this.score = 0;
+    this.word.guessArray = [];
+
+    // Append the wins/losses/counter elements to the page.
+    status.appendChild(winsElement);
+    status.appendChild(losesElement);
+    status.appendChild(counterElement);
+
+    // Start the counter for the next game.
+    setInterval(function () {
+
+        // Every second deincriment the counter
+        counterSpan.textContent--
+
+            // When counter reaches 3, set the color to red.
+            if (counterSpan.textContent < 4) {
+                counterSpan.style.color = "red";
+            }
+
+    }, 1000);
+
+    // Start a new game after 5 seconds.
+    setTimeout(function () {
+        self.newGame();
+    }, 10000);
+
 }
 
 // Sets a random background image from the hangman.backgrounds array.
@@ -207,24 +386,28 @@ hangman.statusImage = function (selector, status) {
     var winOrLose = status === 'win' ? this.randomize(this.gifs.win) : this.randomize(this.gifs.lose);
     img.src = "https://media.giphy.com/media/" + winOrLose + "/giphy.gif";
     element.innerHTML = "";
+    element.style.display = "block";
     element.appendChild(img);
 }
 
 // Custom listen function tht takes an element, an event and a callback function.
 // I use this custom listener mainly to detect the animationend event.
-hangman.listen = function (selector, event, callback) {
+hangman.animationListener = function (selector, event, callback) {
     var element = document.querySelector(selector);
     element.addEventListener(event, callback, false);
+}
+
+hangman.audioElement = function () {
+    var audioElement = document.createElement("audio");
+    audioElement.src = "http://8tracks.s3.amazonaws.com/tf/028/273/276/25013.mp3"
+    audioElement.loop = true;
+    audioElement.play();
 }
 
 /* ------ INITIALIZE GAME ------ */
 
 //Start a new game
 hangman.startGame(hangman.gameElement);
-
-hangman.listen("h1.title", "animationend", function (param) {
-    console.log("Animation complete:", param.animationName);
-});
 
 // Set a random background everytime the page loads.
 hangman.setBackground("div.container");
